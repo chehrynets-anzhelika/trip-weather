97cba9ab6a81baa0af623a670f8b86004c4c9c4e
Lika4y <guralika@gmail.com>
2024-04-10 15:36:42 +0300

    persist added

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,45 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import modalReducer from "./modalSlice";
 import dataReducer from "./dataSlice";
 import tripReducer from "./tripSlice";
 import weatherTodayReducer from "./weatherTodaySlice";
 import searchReducer from "./searchSlice";
 
+const rootReducer = combineReducers({
+    modal: modalReducer,
+    data: dataReducer,
+    currentTrip: tripReducer,
+    weatherToday: weatherTodayReducer,
+    search: searchReducer,
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-    reducer: {
-        modal: modalReducer,
-        data: dataReducer,
-        currentTrip: tripReducer,
-        weatherToday: weatherTodayReducer,
-        search: searchReducer,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
 
+export const persistor = persistStore(store);
 export default store;
