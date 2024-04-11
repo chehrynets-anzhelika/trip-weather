36aa1474f810522babd768adec431979e06dff2d
Lika4y <guralika@gmail.com>
2024-04-11 18:23:45 +0300

    validation in the modal window changed

--- src/handlers/checkDate.js
+++ src/handlers/checkDate.js
@@ -1,8 +1,8 @@
-export function isWithin15Days(dateString) {
+export function isWithin14Days(dateString) {
     const selectedDate = new Date(dateString);
     const currentDate = new Date();
     const diffInMilliseconds = selectedDate - currentDate;
     const diffInDays = diffInMilliseconds / (1000 * 60 * 60 * 24);
     console.log(diffInDays);
-    return diffInDays <= 15;
+    return diffInDays <= 13;
 }
