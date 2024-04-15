7112985701c0c27545e1235f59ef22ea15780aa6
Lika4y <guralika@gmail.com>
2024-04-15 12:36:26 +0300

    Loader added

--- src/components/TripList/tripList.jsx
+++ src/components/TripList/tripList.jsx
@@ -2,31 +2,34 @@ import React from 'react';
 import TripItem from '../TripItem/tripItem';
 import { useSelector } from "react-redux";
 import "./tripList.css";
-import   Slider  from "../Slider/Slider";
+import Slider from "../Slider/Slider";
+
 
 const TripList = () => {
     const trips = useSelector(state => state.data.trips);
     const filteredTrips = useSelector(state => state.data.filteredTrips);
     const searchValue = useSelector(state => state.search.searchValue);
     const displayTrips = !searchValue ? trips : filteredTrips;
+    
 
 
     return (
         <>
-            <Slider cards={displayTrips}>
+            {!trips.length ? <p className='message-card'>You haven’t created any trips yet</p> : <Slider cards={displayTrips}>
                  {displayTrips.map((trip, idx) => (
                         <div key={idx} className="card">
-                            <TripItem
+                           <TripItem
                                 id={trip.city.id}
                                 selected={trip.selected}
                                 cityImage={trip.cityImage}
                                 cityName={trip.city.city}
                                 city={trip.city.city}
                                 startDate={trip.startDate}
-                                endDate={trip.endDate} />
+                                endDate={trip.endDate}
+                                displayTrips={displayTrips} />
                         </div>
                     ))}
-            </Slider>
+            </Slider>}
         </>
     );
 }
